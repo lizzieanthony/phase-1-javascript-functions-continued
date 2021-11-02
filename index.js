f91dfa25e3546905c41915afe079b1a4de44b9b6
Lizzie Anthony <lizziewrab@me.com>
2021-11-02 17:31:57 -0500

    commit tree 80f2fc01a2c9216dd8c0fd79933419ff7d70ad18

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+function saturdayFun(fun = 'roller-skate') {
+    return(`This Saturday, I want to ${fun}!`)
+}
+function mondayWork(monday = 'go to the office') {
+    return (`This Monday, I will ${monday}.`)
+}
+console.log(mondayWork ("work from home"))
+
+function wrapAdjective(wrap = "*") {
+    let newFunction = function(hi){
+        return `You are ${wrap}${hi}${wrap}!`
+    }
+    return newFunction
+}
+//     return (default, inner = "a hard worker"); 
+//     return `You are ${defaul}!`
